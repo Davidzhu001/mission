4d5435c42141378758197e4a010e9d3c5f14c1a5
Davidzhu001 <qluoluo@icloud.com>
2015-05-22 12:58:06 -0500

    git image
    git image

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,5 @@
+//= require jednotka/jquery/jquery.min
+//= require jednotka/jquery/jquery.mobile.custom.min
+//= require jednotka/bootstrap/bootstrap.min
+//= require jednotka/plugins/_plugins
+//= require jednotka/jednotka
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def home
+  	render :layout => 'layout2'
+  end
+
+  def index
+  end
+end

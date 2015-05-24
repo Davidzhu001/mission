1d3e1aad59da9401c8936622ad49fc74fc182c77
Davidzhu001 <qluoluo@icloud.com>
2015-05-24 13:30:05 -0500

    changer
    changer

--- app/assets/javascripts/jednotka/style-switcher.js
+++ app/assets/javascripts/jednotka/style-switcher.js
@@ -4,7 +4,7 @@
   $(document).ready(function() {
     var cls, css_path, lls, sw;
     css_path = "assets/stylesheets/";
-    sw = $("#style-id");
+    sw = $("#style-cl");
     sw.find(".fa-icon-user").click(function() {
       return sw.toggleClass("open");
     });
